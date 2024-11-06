a6fecdc972c25b5ce6291b104e5f0e83c93e3999
Sebastian Buczyński <nnplaya@gmail.com>
2024-11-06 22:35:47 +0100

    Add empty first test for SubscriptionsFacade tests

--- tests/subscriptions/test_facade.py
+++ tests/subscriptions/test_facade.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 import pytest
 from lagom import Container
 
@@ -7,15 +9,28 @@ from subscriptions.subscriptions._facade import SubscriptionsFacade
 
 
 @pytest.fixture()
-def facade(container: Container) -> SubscriptionsFacade:
+def payments_facade() -> Mock:
+    return Mock(spec_set=PaymentsFacade)
+
+
+@pytest.fixture()
+def plans_facade() -> Mock:
+    return Mock(spec_set=PlansFacade)
+
+
+@pytest.fixture()
+def facade(
+    container: Container, payments_facade: Mock, plans_facade: Mock
+) -> SubscriptionsFacade:
     cloned_container = container.clone()
-    # Replacing dependencies using container
-    # stub or mock?
-    cloned_container[PaymentsFacade] = object()  # type: ignore
-    # stub or mock?
-    cloned_container[PlansFacade] = object()  # type: ignore
+    # mock or stub?
+    cloned_container[PaymentsFacade] = payments_facade
+    # mock or stub?
+    cloned_container[PlansFacade] = plans_facade
     return cloned_container.resolve(SubscriptionsFacade)
 
 
-def test_tbd(facade: SubscriptionsFacade) -> None:
+def test_triggers_payment_for_a_new_subscription_based_on_plans_response(
+    facade: SubscriptionsFacade,
+) -> None:
     pass
