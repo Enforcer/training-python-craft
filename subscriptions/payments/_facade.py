4899e13b746b7ebcbf5dca5c38ad1cb0768c7729
Sebastian Buczyński <nnplaya@gmail.com>
2024-11-07 08:48:16 +0100

    Implement charge_with_first_available_method in StripeGateway

--- subscriptions/payments/_facade.py
+++ subscriptions/payments/_facade.py
@@ -1,3 +1,5 @@
+from typing import assert_never
+
 import stripe
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -5,7 +7,13 @@ from sqlalchemy.orm import Session
 from subscriptions.main import payments_settings
 from subscriptions.payments._customer import Customer
 from subscriptions.payments._payment import Payment
-from subscriptions.payments._stripe_gateway import StripeGateway
+from subscriptions.payments._stripe_gateway import (
+    StripeGateway,
+    Success,
+    Failure,
+    NeedsAuthentication,
+    NoPaymentMethods,
+)
 from subscriptions.shared.account_id import AccountId
 from subscriptions.shared.money import Money
 
@@ -57,52 +65,36 @@ class PaymentsFacade:
     def charge(self, account_id: AccountId, amount: Money) -> bool:
         stmt = select(Customer).filter(Customer.account_id == account_id)
         customer = self._session.execute(stmt).scalars().one()
-        methods = self._client.payment_methods.list(
-            params=dict(customer=customer.stripe_customer_id, type="card")
+        result = self._stripe_gateway.charge_with_first_available_method(
+            customer.stripe_customer_id, amount
         )
-        if not methods:
-            raise Exception("No payment methods available")
-
-        selected_method_id = methods.data[0].id
-        try:
-            payment_intent = self._client.payment_intents.create(
-                params=dict(
-                    amount=int(amount.amount * 100),
-                    currency=amount.currency.lower(),
-                    automatic_payment_methods={"enabled": True},
-                    customer=customer.stripe_customer_id,
-                    payment_method=selected_method_id,
-                    off_session=True,
-                    confirm=True,
+        match result:
+            case Success():
+                payment = Payment(
+                    tenant_id=customer.tenant_id,
+                    account_id=account_id,
+                    amount=amount,
+                    status="success",
+                    stripe_payment_id=result.payment_intent_id,
                 )
-            )
-            payment = Payment(
-                tenant_id=customer.tenant_id,
-                account_id=account_id,
-                amount=amount,
-                status="success",
-                stripe_payment_id=payment_intent.id,
-            )
-            self._session.add(payment)
-            self._session.commit()
-            return True
-        except stripe.CardError as exc:
-            error_object = exc.error
-            if error_object is None:
-                raise InternalError("Payment failed and no details are not available")
-            # `error_object.code` will be "authentication_required"
-            # if authentication is needed
-            if error_object.payment_intent is None:
-                raise InternalError("Payment failed and no payment intent is available")
-
-            payment_intent_id = error_object.payment_intent["id"]
-            payment = Payment(
-                tenant_id=customer.tenant_id,
-                account_id=account_id,
-                amount=amount,
-                status="failure",
-                stripe_payment_id=payment_intent_id,
-            )
-            self._session.add(payment)
-            self._session.commit()
-            return False
+                self._session.add(payment)
+                self._session.commit()
+                return True
+            case Failure():
+                payment = Payment(
+                    tenant_id=customer.tenant_id,
+                    account_id=account_id,
+                    amount=amount,
+                    status="failure",
+                    stripe_payment_id=result.payment_intent_id,
+                )
+                self._session.add(payment)
+                self._session.commit()
+                return False
+            case NeedsAuthentication():
+                pass  # no handling YET
+                return False
+            case NoPaymentMethods():
+                raise Exception("No payment methods!")
+            case _:
+                assert_never(result)
--- subscriptions/payments/_stripe_gateway.py
+++ subscriptions/payments/_stripe_gateway.py
@@ -1,8 +1,73 @@
+from dataclasses import dataclass
+
 import stripe
 
 from subscriptions.main import StripeApiKey
+from subscriptions.shared.money import Money
+
+
+class InternalError(Exception):
+    pass
+
+
+@dataclass(frozen=True)
+class Success:
+    payment_intent_id: str
+
+
+@dataclass(frozen=True)
+class NeedsAuthentication:
+    payment_intent_id: str
+
+
+@dataclass(frozen=True)
+class Failure:
+    payment_intent_id: str
+
+
+@dataclass(frozen=True)
+class NoPaymentMethods:
+    pass
 
 
 class StripeGateway:
     def __init__(self, stripe_api_key: StripeApiKey) -> None:
         self._client = stripe.StripeClient(api_key=stripe_api_key)
+
+    def charge_with_first_available_method(
+        self, customer_id: str, amount: Money
+    ) -> Success | Failure | NeedsAuthentication | NoPaymentMethods:
+        methods = self._client.payment_methods.list(
+            params=dict(customer=customer_id, type="card")
+        )
+        if not methods:
+            return NoPaymentMethods()
+
+        selected_method_id = methods.data[0].id
+        try:
+            payment_intent = self._client.payment_intents.create(
+                params=dict(
+                    amount=int(amount.amount * 100),
+                    currency=amount.currency.lower(),
+                    automatic_payment_methods={"enabled": True},
+                    customer=customer_id,
+                    payment_method=selected_method_id,
+                    off_session=True,
+                    confirm=True,
+                )
+            )
+            return Success(payment_intent.id)
+        except stripe.CardError as exc:
+            error_object = exc.error
+            if error_object is None:
+                raise InternalError("Payment failed and no details are not available")
+
+            if error_object.payment_intent is None:
+                raise InternalError("Payment failed and no payment intent is available")
+
+            payment_intent_id = error_object.payment_intent["id"]
+
+            if error_object.code == "authentication_required":
+                return NeedsAuthentication(payment_intent_id)
+
+            return Failure(payment_intent_id)
